0663a07ef0203e49109c166bf61cfb5e44c450b9
user.emial <emil.mehraliyev003@gmail.com>
2024-07-29 17:21:06 +0300

    Updated HW solution 40

--- src/main/java/telran/BullsCowsMapImpl.java
+++ src/main/java/telran/BullsCowsMapImpl.java
@@ -5,20 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 public class BullsCowsMapImpl implements BullsCowsService {
-  private Map<Long, Game> games = new HashMap<>();
-  private long nextId = 1;
-	
-  @Override
-	public Long createNewGame() {
-		long gameId = nextId++;
-		String serverSequence = generateServerSequence();
-		Game newGame = new Game(gameId,serverSequence);
-        games.put(gameId, newGame);
-		return gameId;
-	}
+    private Map<Long, Game> games = new HashMap<>();
+    private long nextId = 1;
 
-	private String generateServerSequence() {
-		String digits = "0123456789";
+    @Override
+    public Long createNewGame() {
+        long gameId = nextId++;
+        String serverSequence = generateServerSequence();
+        Game newGame = new Game(gameId, serverSequence);
+        games.put(gameId, newGame);
+        return gameId;
+    }
+
+    private String generateServerSequence() {
+        String digits = "0123456789";
         StringBuilder sequence = new StringBuilder();
         while (sequence.length() < 4) {
             int index = (int) (Math.random() * digits.length());
@@ -30,25 +30,25 @@ public class BullsCowsMapImpl implements BullsCowsService {
         return sequence.toString();
     }
 
-	@Override
-	public List<MoveResult> getResults(long gameId, Move move) {
-		Game game = games.get(gameId);
-		if(game != null) {
-			return game.moveProcess(move);
-		}
-		return null;
-	}
-
-	@Override
-	public Boolean isGameOver(long gameId) {
-		Game game = games.get(gameId);
-		if(game != null) {
-			return game.isFinished();
-		}
-		return false;
-	}
-	public Map<Long, Game> getGames() {
-        return games;
+    @Override
+    public List<MoveResult> getResults(long gameId, Move move) {
+        Game game = games.get(gameId);
+        if (game != null) {
+            return game.moveProcess(move);
+        }
+        return null;
     }
 
+    @Override
+    public Boolean isGameOver(long gameId) {
+        Game game = games.get(gameId);
+        if (game != null) {
+            return game.isFinished();
+        }
+        return false;
+    }
+
+    public Map<Long, Game> getGames() {
+        return games;
+    }
 }
